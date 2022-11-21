adc1e8ab783a21d890a2118969a577281836a450
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-11-20 22:52:02 -0300

    Finalizada função de cadastro do cognito

---
+++ src/functions/auth.ts
@@ -0,0 +1,11 @@
+import { Handler, APIGatewayEvent } from "aws-lambda";
+
+export const register: Handler = async (event: APIGatewayEvent) : Promise<any> => {
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: 200,
+    body: JSON.stringify('Cadastro efetuado com sucesso')
+  }
+}
