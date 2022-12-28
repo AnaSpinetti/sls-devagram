d9bf07ea2bca2fb72ac441fccbbe784df7cbd15a
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-12-28 01:02:23 -0300

    conclusão do endpoint de comentar

--- src/functions/post.ts
+++ src/functions/post.ts
@@ -112,3 +112,52 @@ export const toggleLike: Handler = async (event: any): Promise<DefaultJsonRespon
         return formatDefaultResponse(500, 'Erro ao curtir/descurtir a publicação: ' + e);
     }
 }
+
+export const postComment: Handler = async (event: any): Promise<DefaultJsonResponse> => {
+    try {
+        const { error } = validateEnvs(['POST_TABLE']);
+
+        if (error) {
+            return formatDefaultResponse(500, error);
+        }
+
+        const userId = getUserIdFromEvent(event);
+        if (!userId) {
+            return formatDefaultResponse(400, 'Usuário não encontrado');
+        }
+
+        const user = await UserModel.get({ 'cognitoId': userId });
+        if (!user) {
+            return formatDefaultResponse(400, 'Usuário não encontrado');
+        }
+
+        const {postId} = event.pathParameters;
+        const post = await PostModel.get({id: postId});
+        if (!post) {
+            return formatDefaultResponse(400, 'Publicação não encontrada');
+        }
+        
+        const request = JSON.parse(event.body);
+        const {comment} = request;
+        
+        if(!comment || comment.length < 2){
+            return formatDefaultResponse(400, 'Comentário inválido');
+        }
+
+        const commentObj = {
+            userId,
+            userName: user.name,
+            date: moment().format(),
+            comment
+        }
+        
+        post.comments.push(commentObj);
+        await PostModel.update(post)
+
+        return formatDefaultResponse(200, 'Comentário inserido com sucesso')
+
+    } catch (e: any) {
+        console.log('Error on post comment: ', e);
+        return formatDefaultResponse(500, 'Erro ao comentar a publicação: ' + e);
+    }
+}
