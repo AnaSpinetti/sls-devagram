7d4fd76811253e1b6028ad0edc86e2b761df26f7
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2023-01-02 22:34:29 -0300

    Configuração de logs finalizada

--- src/functions/login.ts
+++ src/functions/login.ts
@@ -3,12 +3,14 @@ import { DefaultJsonResponse, formatDefaultResponse } from "../utils/formatRespo
 import { LoginRequest } from '../types/login/LoginRequest';
 import { CognitoServices } from "../services/CognitoServices";
 import { validateEnvs } from "../utils/environmentsUtils";
+import {logger} from '../utils/loggerUtils';
 
 export const handler: Handler = async (event: APIGatewayEvent): Promise<DefaultJsonResponse> => {
      try {
           const { USER_POOL_ID, USER_POOL_CLIENT_ID, error } = validateEnvs(['USER_POOL_ID', 'USER_POOL_CLIENT_ID']);
 
           if (error) {
+               logger.error("login.handler - " + error)
                return formatDefaultResponse(500, error);
           }
 
@@ -22,13 +24,18 @@ export const handler: Handler = async (event: APIGatewayEvent): Promise<DefaultJ
           if (!login || !password) {
                return formatDefaultResponse(400, 'Parâmetros de entrada inválidos');
           }
-
+          
+          logger.info('login.handler - start: ', login)
+          
           const result = await new CognitoServices(USER_POOL_ID, USER_POOL_CLIENT_ID).login(login, password);
+          
+          logger.debug('login.handler - cognito response: ', result)
+          logger.info('login.handler - finish: ', login)
 
           return formatDefaultResponse(200, undefined, result);
 
      } catch (e: any) {
-          console.log('Error on request login: ', e);
+          logger.error('Login.Handler - Error on request login: ', e);
           return formatDefaultResponse(500, 'Erro ao realizar o login: ' + e);
      }
 }
